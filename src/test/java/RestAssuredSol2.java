c4f3883f127b4d478b69a10639eb48cb758930a7
himanshukumar396 <himanshukumar7@deloitte.com>
2022-03-31 11:57:35 +0530

    Mini Assignment(Rest Assured)

---
+++ src/test/java/RestAssuredSol2.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+import java.io.File;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class RestAssuredSol2 {
+    @Test
+    public void put_call(){
+        File JsonData=new File("src\\test\\resources\\putdata.json");
+        given().
+                baseUri("https://reqres.in/api/users").
+                body(JsonData).
+                header("Content-Type","application/json").
+                when().
+                put("/users").
+                then().
+                statusCode(200)
+                .body("name",equalTo("Arun"))
+                .body("job",equalTo("Manager"));
+    }
+}
